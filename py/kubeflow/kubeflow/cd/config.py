4d83373805f146c09803a36978beefa3df46aa19
DavidSpek <vanderspek.david@gmail.com>
2021-02-23 15:04:31 +0100

    Add R-Studio image

--- py/kubeflow/kubeflow/cd/config.py
+++ py/kubeflow/kubeflow/cd/config.py
@@ -11,3 +11,5 @@ ACCESS_MANAGEMENT_IMAGE = "%s/access-management" % AWS_REGISTRY
 PROFILE_CONTROLLER_IMAGE = "%s/profile-controller" % AWS_REGISTRY
 NOTEBOOK_CONTROLLER_IMAGE = "%s/notebook-controller" % AWS_REGISTRY
 TENSORBOARD_CONTROLLER_IMAGE = "%s/tensorboard-controller" % AWS_REGISTRY
+
+NOTEBOOK_SERVER_RSTUDIO = "%s/notebook-servers/rstudio" % AWS_REGISTRY
